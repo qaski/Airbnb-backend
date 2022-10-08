dd50e652612f902dabdf2d31a4f85b4b765d6104
Natalia <nrsantos@uts.edu.co>
2022-10-08 16:38:36 -0500

    Registro usuarios

---
+++ src/main/resources/static/js/registrar.js
@@ -0,0 +1,10 @@
+async function registrarUsuario(){
+const request = await fecth('api/usuarios',{
+method:'POST',
+headers:{
+'Accept':'aplication/json',
+'Content-Type':'application/json'
+}
+});
+const usuarios = await request.json();
+}
