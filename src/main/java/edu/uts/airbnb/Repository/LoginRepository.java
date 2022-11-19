9a22c855c9c60a6cef80f0da8add7f4b9576f700
Natalia <nrsantos@uts.edu.co>
2022-11-19 17:21:08 -0500

    Prueba

--- src/main/java/edu/uts/airbnb/Repository/LoginRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.uts.airbnb.Repository;
-import edu.uts.airbnb.Models.Login;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoginRepository extends CrudRepository<Login ,Long> {
-}
