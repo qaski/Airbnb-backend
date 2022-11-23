d5a94a0214788a0039631d229bae58723ebb36e5
Natalia <nrsantos@uts.edu.co>
2022-11-23 12:15:53 -0500

    Actividad Api

--- src/main/java/edu/uts/airbnb/AirbnbApplication.java
+++ src/main/java/edu/uts/airbnb/AirbnbApplication.java
@@ -1,9 +1,18 @@
 package edu.uts.airbnb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 @SpringBootApplication
 public class AirbnbApplication {
     public static void main(String[] args) {
         SpringApplication.run(AirbnbApplication.class, args);
+    }
+
+
+    @Bean
+    public RestTemplate restTemplate (){
+        return new RestTemplate();
     }
 }
--- src/main/java/edu/uts/airbnb/Controller/Api_controller.java
+++ src/main/java/edu/uts/airbnb/Controller/Api_controller.java
@@ -1,4 +1,35 @@
 package edu.uts.airbnb.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
 public class Api_controller {
+
+    private final RestTemplate restTemplate;
+    @Autowired
+    public Api_controller (RestTemplate  restTemplate){
+        this.restTemplate = restTemplate;
+
+    }
+
+
+    @GetMapping("/api")
+    public Object api() {
+        String url = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-RapidAPI-Key", "d567afd758msh137dd6bf7812f8ap17616ajsn26a8cdf25c85");
+        headers.set("X-RapidAPI-Host", "wft-geo-db.p.rapidapi.com");
+        headers.set("content-type", "multipart/form-data; boundary=---011000010111000001101001");
+        HttpEntity<String> httpEntity = new HttpEntity<>("", headers);
+        Object forObject = restTemplate.exchange(url, HttpMethod.GET, httpEntity, Object.class);
+        return forObject;
+    }
+
 }
